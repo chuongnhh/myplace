46fb41b555b915f31b1a20d993168f64749c7806
Nguyen Hoang Chuong <chuongnh.hcm@gmail.com>
2017-09-25 13:29:08 +0700

    init app

---
+++ server.js
@@ -0,0 +1,27 @@
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('on_message', function (msg) {
+        //socket.broadcast.emit('emit_message', msg);
+        console.log(msg['id'] + '\nlat: ' + msg['lat'] + ', lng: ' + msg['lng'] + '\n' + msg['address']);
+        io.emit('emit_message', msg);
+    });
+});
+
+http.listen(process.env.PORT || 8080, function () {
+    console.log('listening on *:8080')
+})
