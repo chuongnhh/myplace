52c2758e836bf49132274ad654398af518421f7c
Nguyen Hoang Chuong <chuongnh.hcm@gmail.com>
2017-09-25 13:36:34 +0700

    init app

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+    io.emit('conect_message', 'a user connected');
+    
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
